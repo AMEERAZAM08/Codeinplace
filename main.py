ad7109e8bc9ce4b667ea93bd36f0ce51dd68ab9d
ameer azam <ameerazam08@gmail.com>
2021-04-14 18:11:48 +0000

    x

--- main.py
+++ main.py
@@ -6,5 +6,34 @@ print(y)
 size= len(y)  # is for finding the lenght of string in len 
               # input must be string 
               #to check the type of that input using simple func 
-print(type(x)) # ouput is int class 
-print(type(y)) # output is string class 
+#print(type(x)) # ouput is int class 
+#print(type(y)) # output is string class 
+
+
+#python execution is done in line by line 
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#Casting    If you want to specify the data type of a variable, this can be done with casting.
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x,y,z)
+
+
+#Get the Type You can get the data type of a variable with the type() function.
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#same Quotation
+x = "John"
+# is the same as
+x = 'John'
+
+#look twice then judge
+a = 4
+A = "Sally"
+#A will not overwrite a
