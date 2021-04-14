8c525e0d915de85fb38b9c2f42f3f85cb2387dfd
ameer azam <ameerazam08@gmail.com>
2021-04-14 17:58:45 +0000

    x

---
+++ main.py
@@ -0,0 +1,10 @@
+
+x = 5  #x holding integer number 
+y = "John" # y holding string of len 4
+print(x)
+print(y)
+size= len(y)  # is for finding the lenght of string in len 
+              # input must be string 
+              #to check the type of that input using simple func 
+print(type(x)) # ouput is int class 
+print(type(y)) # output is string class 
